60a0e226d5bb9bee8e45647092146f2eccd80a97
josejosepreso <josehihat@gmail.com>
2025-01-19 20:28:10 -0600

    what

--- Temp.hs
+++ Temp.hs
@@ -2,14 +2,15 @@ import System.IO
 import Control.Concurrent
 import Text.Printf
 
-putInfo :: IO String
-putInfo = show <$> ((*) 1.0e-3 <$> read <$> head . words <$> readFile tempPath :: IO Float)
+putInfo :: IO Float
+putInfo = ((*) atom <$> read <$> head . words <$> readFile tempPath :: IO Float)
 
 main :: IO ()
 main = do
-  putInfo >>= printf "%s deg\n"
+  putInfo >>= printf "%.1f deg\n"
   hFlush stdout
   threadDelay 10000000
   main
 
 tempPath = "/sys/class/hwmon/hwmon3/temp1_input"
+atom = 1.0e-3
--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/sh
-
-[ ! -d "object" ] && mkdir object
-[ ! -d "bin" ] && mkdir bin
-[ ! -d "interface" ] && mkdir interface
-
-for f in *.hs; do ghc -dynamic --make -odir ./object/ -hidir ./interface/ -o ./bin/"$(echo $f | sed 's/.hs//')" $f; done
